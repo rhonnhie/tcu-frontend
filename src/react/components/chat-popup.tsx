5b9b0e45cdb3349314fa1ee35aaf6289ef58324a
rhonnhie <144591275+rhonnhie@users.noreply.github.com>
2023-12-14 17:08:16 +0800

    Update chat-popup.tsx

--- src/react/components/chat-popup.tsx
+++ src/react/components/chat-popup.tsx
@@ -133,7 +133,7 @@ export const ChatPopUp = function ChatPopUp() {
           </button>
         </header>
         <section className='p-4 pb-0'>
-          <div className='assistant-message-container text-sm lg:text-base max-h-[350px] lg:max-h-[350px]'>
+          <div className='assistant-message-container text-sm lg:text-base max-h-[200px] lg:max-h-[200px]'>
             {conversation.map((message, index) => {
               if (message.role === 'assistant') {
                 return (
